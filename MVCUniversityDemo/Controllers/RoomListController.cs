d4af1f5ba0c76cfc8fc68c658539d13506c64b18
fdecaire <frank@decaire.net>
2015-05-24 16:58:46 -0400

    added code

---
+++ MVCUniversityDemo/Controllers/RoomListController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityApp;
+
+namespace MVCUniversityDemo.Controllers
+{
+    public class RoomListController : Controller
+    {
+		private void PopulateViewBagData(string lstRoomSelector)
+		{
+			ClassAPIRoomRequest classAPIRoomRequest = new ClassAPIRoomRequest();
+
+			switch (lstRoomSelector)
+			{
+				case "Show Only Reserved Rooms":
+					classAPIRoomRequest.AllReserved = true;
+					break;
+				case "Show Only Available Rooms":
+					classAPIRoomRequest.AllAvailable = true;
+					break;
+			}
+
+			var classRoomReservationList = new ClassRoomReservationList(classAPIRoomRequest);
+			ViewBag.ClassRoomReservationList = classRoomReservationList;
+
+			var listItems = new List<string> { 
+				"Show All Rooms",
+				"Show Only Reserved Rooms",
+				"Show Only Available Rooms"
+			};
+
+			ViewBag.RoomList = new SelectList(listItems);
+		}
+
+        //
+        // GET: /RoomList/
+		public ActionResult Index()
+		{
+			PopulateViewBagData("");
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult ChangeSelection(string lstRoomSelector)
+		{
+			PopulateViewBagData(lstRoomSelector);
+			return View("Index");
+		}
+    }
+}
